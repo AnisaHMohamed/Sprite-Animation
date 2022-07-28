9cee8883c9bb8763b8bb4c1e7aefd37421928436
AnisaHMohamed <anisahamohamed@gmail.com>
2022-07-28 16:14:32 -0400

    refractor: animation position

--- script.js
+++ script.js
@@ -8,19 +8,17 @@ playerImage.src ='./assets/shadow_dog.png'
 const spriteWidth = 575;
 const spriteHeight = 523;
 let frameX = 0;
-let frameY = 1;
+let frameY = 0;
 let gameFrame = 0;
+// higher stagger frame slower the animation
 const staggerFrame = 5;
 
 const animate = () => {
-const CANVAS_WIDTH = canvas.width = 600;
+let position = Math.floor(gameFrame/staggerFrame) % 6;
+frameX = spriteWidth * position
 ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-// ctx.drawImage(image, sx, sy, sw, sh, dx,dy,dw,dh);
-ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-if(gameFrame % staggerFrame == 0){
-    if (frameX < 6) frameX ++;
-    else frameX = 0;
-    }
+ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
 gameFrame++;
 requestAnimationFrame(animate);
 }
