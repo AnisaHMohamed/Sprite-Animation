736b67f90721232faa7e9da05eb80cf391c55b66
AnisaHMohamed <anisahamohamed@gmail.com>
2022-07-28 15:42:39 -0400

    indivial sprite cut out

--- script.js
+++ script.js
@@ -5,11 +5,14 @@ const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
 playerImage.src ='./assets/shadow_dog.png'
+const spriteWidth = 575;
+const spriteHeight = 523;
+
 const animate = () => {
 const CANVAS_WIDTH = canvas.width = 600;
 ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-ctx.fillRect(100, 50, 100, 100);
-ctx.drawImage(playerImage, 0, 0);
+// ctx.drawImage(image, sx, sy, sw, sh, dx,dy,dw,dh);
+ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 requestAnimationFrame(animate);
 }
 animate()
