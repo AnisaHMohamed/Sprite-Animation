ef74c0fdb980cc4f3f3c4ab55d412c6de7e08ae7
AnisaHMohamed <anisahamohamed@gmail.com>
2022-07-28 15:48:33 -0400

    use frame to change sprite image

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ const playerImage = new Image();
 playerImage.src ='./assets/shadow_dog.png'
 const spriteWidth = 575;
 const spriteHeight = 523;
+let frameX = 0;
+let frameY = 0;
 
 const animate = () => {
 const CANVAS_WIDTH = canvas.width = 600;
 ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 // ctx.drawImage(image, sx, sy, sw, sh, dx,dy,dw,dh);
-ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 requestAnimationFrame(animate);
 }
 animate()
